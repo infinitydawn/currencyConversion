ebaaf80b126a10b233cfa1cc65cd53058a844c13
Mikhail Nikulin <nklnmish@gmail.com>
2024-01-05 13:35:00 -0500

    updated currency descriptions and custom input value

--- index.js
+++ index.js
@@ -6,11 +6,29 @@ const targetCurrency = document.querySelector("#target-currency")
 const dropdowns = document.querySelectorAll(".grid select")
 const convResult = document.querySelector("#conversion-result")
 const exchangeAPI = new FetchWrapper("https://v6.exchangerate-api.com/v6/946c56a1a142115724780c45/latest/")
+//let currencies = document.querySelectorAll("#base-currency option");
+let targetValue;
+let inputValue = document.querySelector("#input-amount")
 
+//console.log(currencies)
 dropdowns.forEach(dropdown => {
     dropdown.addEventListener("change", event => {
-        console.log("test")
         exchangeAPI.get(baseCurrency.value)
-        .then(data => convResult.textContent = data.conversion_rates[targetCurrency.value])
+            .then(data =>{ 
+                targetValue = data.conversion_rates[targetCurrency.value];
+                updateResult(targetValue);}
+            )
     })
 })
+
+
+inputValue.addEventListener("change", event => {
+    // update target curr value
+    let newValue = `${parseInt(targetValue) * inputValue.value}`
+    updateResult(newValue);
+})
+
+
+function updateResult(textValue){
+    convResult.textContent = `${textValue}`;
+}
